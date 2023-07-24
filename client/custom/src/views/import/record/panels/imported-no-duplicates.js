88a0b5923462329d480999c581290f4bf1ac3140
Alex Howes <howesalexandra@gmail.com>
2023-07-24 10:13:21 +0000

    Add a panel to the import results page to show the list of data that was imported but not including duplicates.

---
+++ client/custom/src/views/import/record/panels/imported-no-duplicates.js
@@ -0,0 +1,24 @@
+/************************************************************************
+ * Added to show a new panel on the Import results/ detail page, 
+ * showing the list of data that was imported, but not including duplicates.
+ * 
+ * Based on file: "client/src/views/import/record/panels/imported.js"
+ * Link set to: "importedNoDuplicates"
+ ************************************************************************/
+
+define('custom:views/import/record/panels/imported-no-duplicates', ['views/record/panels/relationship'], function (Dep) {
+
+    return Dep.extend({
+
+        link: 'importedNoDuplicates',
+        readOnly: true,
+        rowActionsView: 'views/record/row-actions/relationship-no-unlink',
+
+        setup: function () {
+            this.scope = this.model.get('entityType');
+            this.title = this.title || this.translate('importedNoDuplicates', 'labels', 'Import');
+
+            Dep.prototype.setup.call(this);
+        },
+    });
+});
