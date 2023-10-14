dedd2e99a33d259240e8c6b9a3cf57dfab375afc
Alex Howes <howesalexandra@gmail.com>
2023-10-14 16:50:13 +0000

    Add standard files for the DuplicateCheck entity which are auto-generated by espocrm when creating entities in the front-end

---
+++ custom/Espo/Custom/Repositories/DuplicateCheck.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Espo\Custom\Repositories;
+
+class DuplicateCheck extends \Espo\Core\Templates\Repositories\Base
+{
+}
---
+++ custom/Espo/Custom/Services/DuplicateCheck.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Espo\Custom\Services;
+
+class DuplicateCheck extends \Espo\Core\Templates\Services\Base
+{
+}
