941565e0cbc71d9c620b894f9d88fc7d157f931d
Alex Howes <howesalexandra@gmail.com>
2023-10-14 18:49:01 +0000

    Add files which are auto generated when creating the new CashDistribution entity

---
+++ custom/Espo/Custom/Entities/CashDistribution.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Espo\Custom\Entities;
+
+class CashDistribution extends \Espo\Core\Templates\Entities\Base
+{
+    public const ENTITY_TYPE = 'CashDistribution';
+
+    protected $entityType = 'CashDistribution';
+}
---
+++ custom/Espo/Custom/Repositories/CashDistribution.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Espo\Custom\Repositories;
+
+class CashDistribution extends \Espo\Core\Templates\Repositories\Base
+{
+}
