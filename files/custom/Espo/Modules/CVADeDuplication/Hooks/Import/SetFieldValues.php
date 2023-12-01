a101adb573a4d9ca785dae3405610ddc7acc1b92
Wessel de Jong <wes_dejong9@hotmail.com>
2023-12-01 13:46:57 +0100

    Set import name to translation

--- files/custom/Espo/Modules/CVADeDuplication/Hooks/Import/SetFieldValues.php
+++ files/custom/Espo/Modules/CVADeDuplication/Hooks/Import/SetFieldValues.php
@@ -24,18 +24,9 @@ class SetFieldValues
     public function beforeSave(Entity $entity, array $options): void
     {
         if ($entity->isNew()) {
-
-            // Set the action field based on the entity type
-            $actionFieldValues = array(
-                "CashDistribution" => $this->language->translate("CashDistribution", 'importEntityDescriptive', 'Import'),
-                "DuplicateCheck" => $this->language->translate("DuplicateCheck", 'importEntityDescriptive', 'Import'),
-            );
             $entityType = $entity->get('entityType');
             if (!empty($entityType)) {
-                if (array_key_exists($entityType, $actionFieldValues)) {
-                    $entity->set('action', $actionFieldValues[$entityType]);
-                }
-                $entity->set('action', $entityType);
+                $entity->set('action', $this->language->translate($entityType, 'importEntityDescriptive', 'Import'));
             }
         }
     }
